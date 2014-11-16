dc001759b61747dae90db0b36e5674fa1cbeb695
Carl Myers <cmyers@palantir.com>
2014-11-16 12:49:33 -0800

    Fixing to be easier to run gitit.

---
+++ bin/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# TO USE: 
+# mkdir cooking
+# cd cooking
+# git clone <...> git
+# git/bin/start.sh
+if [[ ! -e static ]];
+then
+    ln -s git/wiki-config/static static
+fi
+
+if [[ ! -e templates ]];
+then
+    ln -s git/wiki-config/templates templates
+fi
+
+if [[ ! -e my.conf ]];
+then
+    ln -s git/wiki-config/my.conf my.conf
+fi
+
+gitit -f my.conf
